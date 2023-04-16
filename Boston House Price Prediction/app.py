3331a457f584c081bc1028bcfe2d0cf79464c249
code-with-praveen143 <codethebug3@gmail.com>
2023-04-16 17:26:01 +0530

    My Machine Learning Model

--- Boston House Price Prediction/app.py
+++ Boston House Price Prediction/app.py
@@ -1,32 +1,26 @@
+import json
 import pickle
-from flask import Flask,render_template,request,app,jsonify,url_for
-import pandas
-import numpy
-import os
 
-app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
 
-model = pickle.load(open("model.pkl",'rb'))
-scalar = pickle.load(open("scaling.pkl",'rb'))
-
-
-
-@app.route("/home")
-
+app=Flask(__name__,template_folder='template')
+## Load the model
+model=pickle.load(open('model.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House Price Prediction is {}".format(output))
 
-
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=port)
+if __name__=="__main__":
+    app.run(debug=True)
+   

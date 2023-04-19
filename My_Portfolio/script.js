fd1ce216e9d9580e2734d73ad0e82de05bc575c4
code-with-praveen143 <116370782+code-with-praveen143@users.noreply.github.com>
2023-04-19 15:39:18 +0530

    Added files via upload

--- My_Portfolio/script.js
+++ My_Portfolio/script.js
@@ -1,37 +1,33 @@
-$(document).ready(function(){
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-bars');
-        $('header').toggleClass('toggle');
+/* MENU SHOW */ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
 
-});
-    $(window).on('scroll load',function(){
-        $('#menu').toggleClass('fa-bars');
-        $('header').toggleClass('toggle');
+showMenu('nav-toggle','nav-menu')
 
-    });
-    $(window).on('scroll load',function(){
-        $('#menu').removeClass('fa-bars');
-        $('header').removeClass('toggle');
-        if($(window).scrollTop() > 0){
-            $('.top').show();
-        }
-        else{
-            $('.top').hide();
-        }
-    });
+/*----- ANIMATE -----*/
+// OVERLAY
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
+// IMG
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
 
-    $('a[href*="#"]').click(function(e){
+// INFORMATION
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
 
+// NAV ITEM
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
 
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop : $($(this).after('href')).offset().top,
-        },
-
-        500,
-        'linear'
-        );
-    });
-});
+// SOCIAL
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})

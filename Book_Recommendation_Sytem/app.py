3151a6cfd7580f8de364b9433077123de524e274
code-with-praveen143 <codethebug3@gmail.com>
2023-04-28 14:55:21 +0530

    Add files via upload

--- Book_Recommendation_Sytem/app.py
+++ Book_Recommendation_Sytem/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
-popular_df = pickle.load(open('model.pkl','rb'))
+popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
 books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))

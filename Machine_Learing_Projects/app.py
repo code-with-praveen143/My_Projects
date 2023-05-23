074c74052332fa546c024a203a03e9b609ca0c63
code-with-praveen143 <codethebug3@gmail.com>
2023-05-23 18:10:39 +0530

    commit

---
+++ Machine_Learing_Projects/app.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,render_template
+import numpy as np
+import os
+import pandas as pd
+import pickle
+with open('model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+from sklearn.preprocessing import StandardScaler
+
+app=Flask(__name__)
+
+port=int(os.environ.get("PORT",5000))
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return "<h2>Hello Friends</h2>"
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=pd.DataFrame(data)
+        print(pred_df)
+        print("Before Prediction")
+
+        # predict_pipeline=PredictPipeline()
+        # print("Mid Prediction")
+        results=loaded_model.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0',port=port)

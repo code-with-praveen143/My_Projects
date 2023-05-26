0d59ea8a90739ec99ce506aea884111bd0dd86e5
code-with-praveen143 <codethebug3@gmail.com>
2023-05-26 17:52:55 +0530

    Add files via upload

--- Boston House Price Prediction/app.py
+++ Boston House Price Prediction/app.py
@@ -1,25 +1,27 @@
 import json
 import pickle
-
+import sklearn
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__,template_folder='template')
+app=Flask(__name__,template_folder='templates')
 ## Load the model
-model=pickle.load(open('model.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+model=pickle.load(open('house.pkl','rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House Price Prediction is {}".format(output))
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    flask_prediction = model.predict(final_features)
+    print(flask_prediction)
+    return render_template("home.html",prediction_text=flask_prediction)
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ Boston House Price Prediction/model.py
@@ -0,0 +1,58 @@
+#Importing Dependencies 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+import sklearn
+#load the dataset
+from sklearn.datasets import load_boston
+boston = load_boston()
+print(boston.keys())
+dataset = pd.DataFrame(boston.data,columns = boston.feature_names)
+print(dataset.head())
+dataset['Price'] = boston.target
+# Independent And Dependent Features 
+X = dataset.iloc[:,:-1]
+Y = dataset.iloc[:,-1]
+# Splitting the data into training and testing data
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 0.3,random_state=0)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+#Load the Machine Learning Model
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor()()
+model.fit(X_train,Y_train)
+# Prediction on train data
+train_pred = model.predict(X_train)
+# Prediction on test data
+test_pred = model.predict(X_test)
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+print('mean_absolute_error: ',mean_absolute_error(Y_test,test_pred))
+print('mean_squared_error: ',mean_squared_error(Y_test,test_pred))
+print('root_mean_squared_error: ',np.sqrt(mean_squared_error(Y_test,test_pred)))
+from sklearn.metrics import r2_score
+r2score = r2_score(Y_test,test_pred)
+print("R^2 Score: ",r2score)
+adjusted_r2_score = 1-(1-r2score)*(len(Y_test)-1)/(len(Y_test)-X_test.shape[1]-1)
+print("Adjusted R^2 Score: ",adjusted_r2_score)
+input_data = (0,18,2,0,0,6,65,4,1,296,15,396,4.98)
+
+# converting the input data into a numpy array
+input_data_as_array = np.asarray(input_data)
+
+# reshaping the input data into 2-D array
+input_data_reshaped = input_data_as_array.reshape(1,-1)
+
+# transforming the data by using StandardScaler
+scaled_data = scaler.fit_transform(input_data_reshaped)
+
+# predciting the outcome on new scaled input data
+prediction = model.predict(scaled_data)
+print(prediction)
+
+import pickle
+pickle.dump(model,open('house.pkl','wb'))

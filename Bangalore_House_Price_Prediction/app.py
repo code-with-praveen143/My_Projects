50bfff23f0e107739b9236c52c1a2f4d2173240d
code-with-praveen143 <codethebug3@gmail.com>
2023-05-30 10:33:44 +0530

    commit

--- Bangalore_House_Price_Prediction/app.py
+++ Bangalore_House_Price_Prediction/app.py
@@ -1,13 +1,19 @@
 from flask import *
 import numpy as np
 import os
+import pandas as pd
+
+data = pd.read_csv("Cleaned_data.csv")
+
 
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT",3000))
 @app.route('/')
 def home():
-    return render_template('index.html')
+    locations = sorted(data['location'].unique())
+    return render_template('index.html',locations=locations)
+
 
 
 if __name__ == "__main__":
